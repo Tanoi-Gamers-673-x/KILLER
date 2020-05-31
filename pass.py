78d78e80a1980843b0c5849ee79155d98665c369
Tanoi-Gamers-673-x <62834829+Tanoi-Gamers-673-x@users.noreply.github.com>
2020-05-31 20:34:04 +0700

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,27 @@
+import os,sys
+import time
+
+print ("Login..»")
+pas = input("\033[31mkiller@673x\033[36m:\033[34mLogin\033[32m# ")
+if pas == "673x2600":
+ print ("[01]>[The most dangerous virus]")
+ print ("[02]>[PRIVATE OF CLAN APT ~]")
+ print ("[00]>[Exit To HOME KILLER]")
+ ex = input("\033[31mkiller@673x\033[36m:\033[34mDate KILLER\033[32m# ")
+if ex == "01" or ex == "1":
+ os.system ("clear")
+ print ("\033[31m")
+ os.system ("figlet Virus")
+ print ("«Code Virus»")
+ v1 = """\033[31m
+  «Virus on system linux»
+  yes Hacker673x > Hackpass.txt
+  """
+ print (v1)
+elif ex == "02" or ex == "2":
+ os.system ("clear")
+ print ("\033[31m")
+ os.system ("figlet PRIVATE")
+ print ("«Exit to main menu KILLER >type in >[killer]»")
+ ss = input("\033[31mkiller@673x\033[36m:\033[34m[killer]\033[32m# ")
+ os.system (" " + (ss))
