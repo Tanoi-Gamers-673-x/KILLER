ff179fca1dda7ccf57d9af0937902fb8fdfe0aef
Tanoi-Gamers-673-x <62834829+Tanoi-Gamers-673-x@users.noreply.github.com>
2020-05-31 17:46:09 +0700

    Update qu2.py

--- qu2.py
+++ qu2.py
@@ -14,7 +14,7 @@ print ("    [03]>[Virus Make the machine slow]")
 print ("    [04]>[Virus Virus Shut down every 5 seconds]")
 print ("    [05]>[Virus Broken internet]")
 print ("    [06]>[Virus Delet Windown]")
-print ("    [07]>[Virus Fromat all drives]
+print ("    [07]>[Virus Fromat all drives]")
 print ("    [00]>[Black To HOME KILLER ]")
 print (" \033[34m ")
 print
